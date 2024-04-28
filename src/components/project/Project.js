63f27bf0b206002fc59fbd7425ce7c99eb4644e5
VadimLNN <svobodolubsev@inbox.ru>
2024-04-28 18:43:39 +0700

    фикс ошибки

--- src/components/project/Project.js
+++ src/components/project/Project.js
@@ -5,14 +5,12 @@ const Project = (props) => {
     return (
         <NavLink to={`/project/${props.index}`}>
             <li className="project">
-                <a href="./project-page.html">
-                    <img
-                        src={props.img}
-                        alt={props.title}
-                        className="project__img"
-                    />
-                    <h3 className="project__title">{props.title}</h3>
-                </a>
+                <img
+                    src={props.img}
+                    alt={props.title}
+                    className="project__img"
+                />
+                <h3 className="project__title">{props.title}</h3>
             </li>
         </NavLink>
     );
