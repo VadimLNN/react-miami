5465fdbaff8f301e26a86f26bfe599ac1678560b
VadimLNN <svobodolubsev@inbox.ru>
2024-04-28 19:41:52 +0700

    подкючение утилки по обнаружению темы системы

--- src/components/btnDarkMode/BtnDarkMode.js
+++ src/components/btnDarkMode/BtnDarkMode.js
@@ -1,11 +1,16 @@
 import { useEffect, useRef } from "react";
 import { useLocalStorage } from "../../utils/useLocalStorage";
+import detectDarkMode from "../../utils/detectDarkMode";
+
 import sun from "./../../img/icons/sun.svg";
 import moon from "./../../img/icons/moon.svg";
 import "./style.css";
 
 const BtnDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage("darkMode", "light");
+    const [darkMode, setDarkMode] = useLocalStorage(
+        "darkMode",
+        detectDarkMode()
+    );
     const btnRef = useRef(null);
 
     useEffect(() => {
