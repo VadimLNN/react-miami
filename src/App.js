2ebe27d6b326efd3c37a3512d747c86d3a6aba6a
VadimLNN <svobodolubsev@inbox.ru>
2024-05-29 15:34:37 +0700

    простая работа с redux по "работе с банковским счётом"

--- src/App.js
+++ src/App.js
@@ -10,13 +10,44 @@ import Contacts from "./pages/Contacts";
 import Project from "./pages/Project";
 
 import ScrollToTop from "./utils/scrollToTop";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+    const dispatch = useDispatch();
+    const cash = useSelector((state) => state.cash);
+
+    const addCash = (cash) => {
+        dispatch({ type: "ADD_CASH", payload: cash });
+    };
+    const getCash = (cash) => {
+        dispatch({ type: "GET_CASH", payload: cash });
+    };
     return (
         <div className="App">
             <Router>
                 <ScrollToTop />
                 <Navbar />
+                <div>
+                    <div style={{ fontSize: "3rem" }}>{cash}</div>
+                    <div style={{ display: "flex" }}>
+                        <button
+                            onClick={() => {
+                                addCash(Number(prompt()));
+                            }}
+                        >
+                            {" "}
+                            Пополнить счёт{" "}
+                        </button>
+                        <button
+                            onClick={() => {
+                                getCash(prompt());
+                            }}
+                        >
+                            {" "}
+                            Снять со счёта{" "}
+                        </button>
+                    </div>
+                </div>
 
                 <Routes>
                     <Route path="/" element={<Home />} />
