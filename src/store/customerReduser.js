fe44f89c96ce14d30bdcf832d2307aca7eab485f
VadimLNN <svobodolubsev@inbox.ru>
2024-06-01 15:26:50 +0700

    добавление нового action

--- src/store/customerReduser.js
+++ src/store/customerReduser.js
@@ -4,9 +4,15 @@ const defaultState = {
 
 const ADD_CUSTOMER = "ADD_CUSTOMER";
 const REMUVE_CUSTOMER = "REMUVE_CUSTOMER";
+const ADD_MANY_CUSTOMERS = "ADD_MANY_CUSTOMERS";
 
 export const customerReduser = (state = defaultState, action) => {
     switch (action.type) {
+        case ADD_MANY_CUSTOMERS:
+            return {
+                ...state,
+                customers: [...state.customers, ...action.payload],
+            };
         case ADD_CUSTOMER:
             return {
                 ...state,
@@ -15,9 +21,7 @@ export const customerReduser = (state = defaultState, action) => {
         case REMUVE_CUSTOMER:
             return {
                 ...state,
-                customers: state.customers.filter(
-                    (customer) => customer.id !== action.payload
-                ),
+                customers: state.customers.filter((customer) => customer.id !== action.payload),
             };
         default:
             return state;
@@ -25,6 +29,7 @@ export const customerReduser = (state = defaultState, action) => {
 };
 
 export const addCustomerAction = (payload) => ({ type: ADD_CUSTOMER, payload });
+export const addManyCustomerAction = (payload) => ({ type: ADD_MANY_CUSTOMERS, payload });
 export const remuveCustomerAction = (payload) => ({
     type: REMUVE_CUSTOMER,
     payload,
