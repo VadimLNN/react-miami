320553e0140c7bffac0827bdeb3a8c80f457546e
VadimLNN <svobodolubsev@inbox.ru>
2024-06-01 15:26:32 +0700

    подключение redux devtools и redux thunk

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { cashReduser } from "./cashReduser";
 import { customerReduser } from "./customerReduser";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
 
 const rootReducer = combineReducers({
     cash: cashReduser,
     customers: customerReduser,
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
