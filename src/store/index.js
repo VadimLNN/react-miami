044840b7385481e768ca1929aa297d2ab4bce3a1
VadimLNN <svobodolubsev@inbox.ru>
2024-06-07 18:44:54 +0700

    функция поновее по совету из подсказки

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
+import { combineReducers, applyMiddleware } from "redux";
 import { cashReduser } from "./cashReduser";
 import { customerReduser } from "./customerReduser";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { thunk } from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
     cash: cashReduser,
     customers: customerReduser,
 });
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = configureStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
