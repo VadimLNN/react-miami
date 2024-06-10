001efb48250f48ea8923a79759c9a479f461538c
VadimLNN <svobodolubsev@inbox.ru>
2024-06-10 15:33:17 +0700

    вернул старое решение

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
-import { combineReducers, applyMiddleware } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { cashReduser } from "./cashReduser";
 import { customerReduser } from "./customerReduser";
 import { composeWithDevTools } from "redux-devtools-extension";
 import { thunk } from "redux-thunk";
-import { configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
     cash: cashReduser,
     customers: customerReduser,
 });
 
-export const store = configureStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
